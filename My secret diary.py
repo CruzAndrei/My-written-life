56c3a6e118e3f75783d10a9948add9fcb0da6e02
CruzAndrei <cruzandrei237@gmail.com>
2023-08-04 11:53:00 +0800

    creating the base of the function

--- My secret diary.py
+++ My secret diary.py
@@ -1,7 +1,12 @@
 #write a method in python to write multiple line of text contents into a text file mylife.txt
+
+
 #open text file entitled "mylife.txt" as write
+def write():
+    with open("mylife.txt", "w") as my_story:
 #Create loop function
 #Create input for the user
 #write in mylife.txt            
 #ask user to continue or not
 #create if-else function
+write()
