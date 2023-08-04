5281ea4c8bd7d3ad5988fb1e216f02e8961c879c
CruzAndrei <cruzandrei237@gmail.com>
2023-08-04 11:51:40 +0800

    psuedocode of the program

---
+++ My secret diary.py
@@ -0,0 +1,7 @@
+#write a method in python to write multiple line of text contents into a text file mylife.txt
+#open text file entitled "mylife.txt" as write
+#Create loop function
+#Create input for the user
+#write in mylife.txt            
+#ask user to continue or not
+#create if-else function
